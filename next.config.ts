ebb78481d33a32d5330b8369eec1fc2e4ece8ae3
shinawatra <shinawatra94@gmail.com>
2025-03-28 11:59:11 +0100

    initial commnit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 };
 
 export default nextConfig;
